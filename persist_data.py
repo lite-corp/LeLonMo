6118403014719a5537200b4b98994f3f91dd0446
Alexis Rossfelder <contact.liteapplication@gmail.com>
2021-01-14 10:06:52 +0100

    update persist_data

--- persist_data.py
+++ persist_data.py
@@ -1,8 +1,6 @@
 import json
 import os
 
-from default_settings import *
-
 save_file = ".\\.save.json"
 
 default_data = dict(
@@ -19,7 +17,8 @@ default_data = dict(
         ACCEPT_ANY_WORD=False,
         ACCEPT_ANY_LETTER=False,
         DEBUG_WORDS=False,
-        SKIP_INTRO=False
+        SKIP_INTRO=False,
+        TEST_VALUE=5
     )
 )
 
@@ -45,9 +44,16 @@ def update_variables(f=save_file):
     global DICT_LANGUAGE
     global USE_INPROVED_GENERATOR
     global GAME_LANGUAGE
+    global ACCEPT_ANY_WORD
+    global ACCEPT_ANY_LETTER
+    global DEBUG_WORDS
+    global SKIP_INTRO
+    global TEST_VALUE
+    global version
 
     settings = get_save()
-    if settings["version"] != version:
+    if settings["version"] != default_data["version"]:
+        update_settings()
         print("Updated from another version, please remove {file} if you encouter any problem"
               .format(file=os.path.abspath(f))
               )
@@ -57,18 +63,38 @@ def update_variables(f=save_file):
         DICT_LANGUAGE = settings["game"]["DICT_LANGUAGE"]
         USE_INPROVED_GENERATOR = settings["settings"]["USE_INPROVED_GENERATOR"]
         GAME_LANGUAGE = settings["settings"]["GAME_LANGUAGE"]
+        ACCEPT_ANY_WORD = settings["debug"]["ACCEPT_ANY_WORD"]
+        ACCEPT_ANY_LETTER = settings["debug"]["ACCEPT_ANY_LETTER"]
+        DEBUG_WORDS = settings["debug"]["DEBUG_WORDS"]
+        SKIP_INTRO = settings["debug"]["SKIP_INTRO"]
+        TEST_VALUE = settings["debug"]["TEST_VALUE"]
+        version = settings["version"]
     except KeyError:
-        print("Error while reading settings, rolling back to default")
+        update_settings()
+
+def update_settings(f=save_file, default=default_data):
+    try:
+        data = get_save(f)
+        for i in default.keys():
+            if i == "version":
+                data[i] = default[i]
+                continue
+            if not data.get(i):
+                data[i] = default[i]
+            for j in default[i]:
+                if not data.get(i).get(j):
+                    data[i][j]=default[i][j]
+    except:
+        print("Error while updating settings, rolling back to default")
         os.remove(f)
         get_save(f)
         print("Please restart the game for the changes to take effect")
-
 
 def write(key, value, f=save_file):
     data = get_save(f)
     data[key] = value
     json.dump(data, open(f, "w"))
-    update_variables()
+    update_variables(f)
 
 
 update_variables()
