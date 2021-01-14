6b7d13040d7a7cf34ebec34e9a02fb8424ad91aa
Alexis Rossfelder <contact.liteapplication@gmail.com>
2021-01-14 08:26:34 +0100

    Use autoformat :) make things better

--- main_offline.py
+++ main_offline.py
@@ -7,9 +7,10 @@ from colors.colors import *
 score_j1 = 0
 score_j2 = 0
 
+
 def human_to_bool(q: str):
     yes = ["oui", "yes", "o", "y"]
-    no  = ["non", "no", "n"]
+    no = ["non", "no", "n"]
     while True:
         r = input(q)
         if r.lower() in yes:
@@ -17,6 +18,7 @@ def human_to_bool(q: str):
         elif r.lower() in no:
             return False
         print(red("La réponse n'est pas valide, entrez oui on non."))
+
 
 def intro():
     global name_j1
@@ -28,41 +30,45 @@ def intro():
             .replace("%%letters%%", bold(letters))
         intro_ln = intro_txt.splitlines(keepends=False)
         for i, l in enumerate(intro_ln):
-            if i in range(4,9):
-                l = l.replace("L", blue("L"))
-                l = l.replace("e", yellow("e"))
-                l = l.replace("o", magenta("o"))
-                l = l.replace("n", cyan("n"))
-                l = l.replace("M", green("M"))
+            if i in range(4, 9):
+                l = l.replace("L", blue("L"))\
+                    .replace("e", yellow("e"))\
+                    .replace("o", magenta("o"))\
+                    .replace("n", cyan("n"))\
+                    .replace("M", green("M"))
             if i == 10:
-                l = l.replace("By Alexis Rossfelder", red("By Alexis Rossfelder"))
+                l = l.replace("By Alexis Rossfelder",
+                            red("By Alexis Rossfelder"))
             print(l)
 
-    print("Les lettres avec lesquelles vous devez composer votre mot sont ")
     name_j1 = input(cyan("Joueur 1, veuiller entrer votre nom : "))
     name_j1 = cyan("Joueur 1" if not name_j1 else name_j1)
     name_j2 = input(magenta("Joueur 2, veuiller entrer votre nom : "))
     name_j2 = magenta("Joueur 2" if not name_j2 else name_j2)
-        
 
 
 def game():
     global score_j1
     global score_j2
 
-    letters = letter_generator.generate((97,123), LETTER_NUMBER, LANGUAGE)
-    
+    letters = letter_generator.generate(
+        (97, 123), LETTER_NUMBER, DICT_LANGUAGE)
+    print("Les lettres avec lesquelles vous devez composer votre mot sont ")
+
     for i in letters:
         print(bold(i), " ", end="")
     print()
-    j1 = getpass(f"{name_j1}, entrez votre mot à l'abris des regards indiscrets : ")
-    while not (word_check.check_dict(j1, LANGUAGE) and word_check.check_list(j1, letters)):
-        j1 = getpass(red("Votre mot n'est pas valide, veuillez en entrer un autre : "))
+    j1 = getpass(
+        f"{name_j1}, entrez votre mot à l'abris des regards indiscrets : ")
+    while not (word_check.check_dict(j1, DICT_LANGUAGE) and word_check.check_list(j1, letters)):
+        j1 = getpass(
+            red("Votre mot n'est pas valide, veuillez en entrer un autre : "))
 
-    j2 = getpass(f"{name_j2}, entrez votre mot à l'abris des regards indiscrets : ")
-    while not (word_check.check_dict(j2, LANGUAGE) and word_check.check_list(j2, letters)):
-        j2 = getpass(red("Votre mot n'est pas valide, veuillez en entrer un autre : "))
-    
+    j2 = getpass(
+        f"{name_j2}, entrez votre mot à l'abris des regards indiscrets : ")
+    while not (word_check.check_dict(j2, DICT_LANGUAGE) and word_check.check_list(j2, letters)):
+        j2 = getpass(
+            red("Votre mot n'est pas valide, veuillez en entrer un autre : "))
 
     print(f"Les mots: \nJ1 : {cyan(j1)}\nJ2 : {magenta(j2)}")
     if len(j1) > len(j2):
@@ -71,17 +77,18 @@ def game():
         print(magenta(f"{name_j2} a gagné"))
     else:
         print(yellow("Egalité parfaite"))
-    
+
     score_j1 += len(j1)
     score_j2 += len(j2)
     print(f"{name_j1} a {score_j1} points et {name_j2} en a {score_j2}. ")
 
+
 def main_loop():
     intro()
     game()
-    while human_to_bool(italic("Voulez-vous rejouer ?\n")): game()
+    while human_to_bool(italic("Voulez-vous rejouer ?\n")):
+        game()
+
 
 if __name__ == "__main__":
     main_loop()
-
-
