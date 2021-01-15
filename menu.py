24b9f851f4b1607e570e9608c336fbfcb7a6e909
LiteApplication <contact.liteapplication@gmail.com>
2021-01-15 08:53:46 +0100

    Add option menu

--- menu.py
+++ menu.py
@@ -3,13 +3,17 @@ from consolemenu import SelectionMenu, MenuFormatBuilder
 from consolemenu.format import MenuBorderStyleType
 import main_offline
 import main_online
+import settings_menu
 from colors.colors import *
 from persist_data import DATA as settings
 
 version = settings["version"]
+
+
 def bye():
     print("Au revoir")
     exit()
+
 
 menu_format = MenuFormatBuilder().set_border_style_type(MenuBorderStyleType.HEAVY_BORDER) \
     .set_prompt(">> ") \
@@ -19,12 +23,19 @@ menu_format = MenuFormatBuilder().set_border_style_type(MenuBorderStyleType.HEAV
     .set_right_margin(4) \
     .show_header_bottom_border(True)
 
-select = ["Mode Local", "Mode en Ligne", "Options", "Quitter"]
-link = [main_offline.main_loop, main_online.main_online,
-        main_online.main_online, bye]
-menu = SelectionMenu(select, f"{blue('L')}{yellow('e')}{blue('L')}{magenta('o')}{cyan('n')}{green('M')}{magenta('o')} {red(f'v{version}')}", subtitle="Le jeu du long mot",
-                prologue_text="Choisisser votre mode de jeu :", show_exit_option=False, formatter=menu_format)
-while True:
-    menu.show()
-    menu.join()
-    link[menu.selected_option]()
+
+def main():
+    select = ["Mode Local", "Mode en Ligne", "Options", "Quitter"]
+    link = [main_offline.main_loop, main_online.main_online,
+            settings_menu.main, bye]
+    menu = SelectionMenu(
+        select, f"{blue('L')}{yellow('e')}{blue('L')}{magenta('o')}{cyan('n')}{green('M')}{magenta('o')} {red(f'v{version}')}",
+        subtitle="Le jeu du long mot",
+        prologue_text="Choisisser votre mode de jeu :",
+        show_exit_option=False,
+        formatter=menu_format
+    )
+    while True:
+        menu.show()
+        menu.join()
+        link[menu.selected_option]()
