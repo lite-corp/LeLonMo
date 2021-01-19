9b7bf8a01997577fa2c82979f2cdf1b4ccd4d2ec
LiteApplication <contact.liteapplication@gmail.com>
2021-01-19 14:00:36 +0100

    Small fix for menus

--- consolemenu/menu_component.py
+++ consolemenu/menu_component.py
@@ -195,7 +195,7 @@ class MenuTextSection(MenuComponent):
             yield self.row()
         if self.text is not None and self.text != '':
             for line in [textwrap.wrap(a, width=self.calculate_content_width(), replace_whitespace=False) for a in self.text.splitlines(False) if a]:
-                yield self.row(content=line, align=self.text_align)
+                yield self.row(content=line[0], align=self.text_align)
         for x in range(0, self.padding.bottom):
             yield self.row()
         if self.show_bottom_border:
