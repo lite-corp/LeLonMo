b551dbe56acab9249c184e40344e11baf14c5810
LiteApplication <contact.liteapplication@gmail.com>
2021-01-21 11:38:39 +0100

    Optimize letter_generator for multiple players

--- letter_generator.py
+++ letter_generator.py
@@ -1,15 +1,16 @@
 from random import randint as ri
 from persist_data import DATA as settings
 
+word_dict = open(f"data/dict/dict_{settings['game']['DICT_LANGUAGE']}.txt", "r", encoding='UTF-8').readlines()
 
-def generate(letter_range=(97, 122), lenght=7, language=settings["game"]["DICT_LANGUAGE"]):
+def generate(letter_range=(97, 122), lenght=settings["game"]["LETTER_NUMBER"], language=settings["game"]["DICT_LANGUAGE"]):
+    global word_dict
     if lenght > 20:
         raise ValueError(f"Trop de lettres ({lenght})")
     if settings["settings"]["USE_INPROVED_GENERATOR"]:
-        d = open(f"data/dict/dict_{language}.txt", "r", encoding='UTF-8').readlines()
         valid = False
         while not valid:
-            shuffle_word = d[ri(1, len(d)-1)].replace('\n', '')
+            shuffle_word = word_dict[ri(1, len(word_dict)-1)].replace('\n', '')
             if settings["debug"]["DEBUG_WORDS"] : print(shuffle_word)
             if len(list(set(list(shuffle_word)))) > lenght :
                 valid=False
