05a0989d60f963ba7af2b67a6046c2284a54afae
Alexis Rossfelder <contact.liteapplication@gmail.com>
2021-02-01 09:38:00 +0100

    Handle KeyboardInterrupt

--- lelonmo/async_input.py
+++ lelonmo/async_input.py
@@ -7,20 +7,20 @@ class Input:
         self.text_shown = str()
         self.hide_char = hide_char
         self.on_update = on_update
-        self.ctrl_pressed = False
     def run(self):    
-        with keyboard.Listener(on_press=self._press, on_release=self.__release) as listener:
+        with keyboard.Listener(on_press=self._press) as listener:
             listener.join()
         return self.text
     def _press(self, key):
         try:
-            if not self.ctrl_pressed:
-                key.char
-                if self.hide_char:
-                    self.text_shown = self.hide_char * (len(self.text)) + key.char
-                else:
-                    self.text_shown = self.text_shown + key.char
-                self.text.append(key.char)
+            key.char # Detect non-char key (ctrl, esc, ...)
+            if key.char == '\x03': # CTRL+C
+                raise KeyboardInterrupt
+            if self.hide_char:
+                self.text_shown = self.hide_char * (len(self.text)) + key.char
+            else:
+                self.text_shown = self.text_shown + key.char
+            self.text.append(key.char)
         except AttributeError:
             if key == keyboard.Key.backspace: # Delete
                 try:
@@ -33,9 +33,4 @@ class Input:
                     self.text_shown = str()
             elif key == keyboard.Key.enter:
                 return False
-            elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r :
-                self.ctrl_pressed = True
         self.on_update(self.text_shown)
-    def __release(self, key):
-        if key == keyboard.Key.ctrl:
-            self.ctrl_pressed = False
