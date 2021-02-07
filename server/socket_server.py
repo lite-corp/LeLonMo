b1b27910763df0e40b20e2a84c494af1bcc0e977
Alexis Rossfelder <contact.liteapplication@gmail.com>
2021-02-07 20:35:30 +0100

    Add points counting, add doc

--- server/socket_server.py
+++ server/socket_server.py
@@ -11,19 +11,35 @@ from server import word_check
 BUFFER_SIZE = 4096
 class Game():
     def __init__(self, LANGUAGE, MAX_PLAYERS = 10):
+        """Initialize the game
+        """
         self.state = 0
         print("[I] Waiting for admin ...")
         self.game_data = dict(players=list())
         self.LANGUAGE = LANGUAGE
         self.MAX_PLAYERS = MAX_PLAYERS
         self.update_file = f'server{os.path.sep}LeLonMo_client_v{persist_data.DATA["client_version"]}.zip'
+        self.scores = {}
 
-    def _new_player(self, uuid, name, ip, status="Connected"):
-        if name in [i['name'] for i in self.game_data["players"]]:
-            print("[V] Refused", uuid, "with name", name)
+    def _new_player(self, uuid: str, name: str, ip: str, status="Connected") -> int:
+        """Add a new player to the game
+        return values : 
+        -5 : game full 
+        -4 : duplicate username
+        -3 : name contains forbidden parts
+        -2 : name.isspace()
+        -1 : empty name
+         0 : success, player is admin
+        >0 : success, player is a normal player
+        """
+        if len(self.game_data["players"]) > self.MAX_PLAYERS:
+            print("[W]Refused", uuid, ": Too many people")
+            return -5
+        elif name in [i['name'] for i in self.game_data["players"]]:
+            print("[W] Refused", uuid, "with name", name)
             return -4
         elif [0 for i in ["admin", "%", "connected", " ", "\t", "\n", "playing"] if i in name.lower()]:
-            print("[V] Refused", uuid, "with name", name)
+            print("[W] Refused", uuid, "with name", name)
             return -3
         elif name.isspace():
             print("[V] Refused", uuid, ": isspace")
@@ -31,16 +47,18 @@ class Game():
         elif name == "":
             print("[V] Refused", uuid, ": Empty name")
             return -1
-        elif len(self.game_data["players"]) > self.MAX_PLAYERS:
-            print("Refused", uuid, ": Too many people")
         else: 
             self.game_data["players"].append(
                 dict(uuid=uuid, name=name, ip=ip, word="", status=status, computer_afk=False))
             print("[I] Added player", name, "with uuid", uuid)
             return len(self.game_data["players"])-1
 
-    def _delete_player(self, player_id: int, admin: bool):
-        if not player_id==-1:
+    def _delete_player(self, uuid: str, admin: bool):
+        """Delete a player if it exists
+        Replace the admin if needed
+        """
+        player_id = self._get_player_id(uuid)
+        if not player_id == -1:
             print(f"[I] Player {self.game_data['players'][player_id]['name']} left.")
             del self.game_data["players"][player_id]
             if admin:
@@ -50,16 +68,21 @@ class Game():
                     self._reset()
 
     def _answer(self, msg, socket):
+        """Transmit data to the client
+        """
         socket.send(bytearray(str(msg), "utf-8"))
         socket.close()
 
     def _get_player_id(self, uuid):
+        "Get the player id from uuid"
         for i, j in enumerate(self.game_data["players"]):
             if j["uuid"] == uuid:
                 return i
         return -1
 
     def _get_results(self):
+        """Dump results to JSON string
+        """
         result = dict(letters=self.game_data["letters"], best=[
                       ("", "")], players=list())
         for i in self.game_data["players"]:
@@ -70,27 +93,47 @@ class Game():
         for i in self.game_data["players"]:
             result["players"].append(dict(
                 name=i["name"],
-                word=i["word"]
+                word=i["word"],
+                points=i["points"],
+                total_points=self.scores[i['uuid']]
             ))
         return json.dumps(result)
 
-    def _get_points(self):
-        results = list()
-        for i in self.game_data["players"]:
-            if not results:
-                results.append(i)
-            for k, j in enumerate(results):
-                if len(i["word"]) > len(j["word"]):
-                    results.insert(k, i)
-        for i, j in enumerate(results):
-            print("j: ", json.dumps(j, indent=2))
+    def _calculate_points(self):
+        """With 5 players:
+            1st has 4 points
+            2nd and 3rd are equal and get 3 points
+            4th gets 1 points
+            5th gets 0 point
+
+        """
+        player_word_len = [(len(j["word"]), i) for i, j in enumerate(self.game_data["players"])]
+        player_word_len.sort(reverse=True)
+
+        word_len = []
+        for j, i in player_word_len:
+            word_len.append(j)
+            self.game_data["players"][i]["points"] =\
+                len(player_word_len) - word_len.index(j) - 1
+            #print(f"[V] Gave {len(player_word_len) - word_len.index(j) - 1} to player {i}") 
+            if self.game_data["players"][i]['uuid'] in self.scores.keys():
+                self.scores[self.game_data["players"][i]['uuid']] += self.game_data["players"][i]["points"]
+            else:
+                self.scores[self.game_data["players"][i]['uuid']] = self.game_data["players"][i]["points"]
 
     def _reset(self):
+        """Restart the game
+        Does not reset points
+        """
         self.state = 0
         print("[I] Reseting game")
         self.game_data = dict(players=list())
 
     def handle_data(self, data: str, client_socket, client_thread):
+        """Main function of the game
+        data : data received from the client in the format
+            "%llm_client%version%uid%message
+        """
         try:
             r = re.search("%llm_client%(.{4})%(.{36})%(.*)", data)
             version = r.group(1)
@@ -128,7 +171,7 @@ class Game():
             except:
                 self._answer("Nobody", client_socket)
         elif msg == "leave%":
-            self._delete_player(self._get_player_id(uuid), admin)
+            self._delete_player(uuid, admin)
         
         # Update sending part
         elif msg == "latest_file%":
@@ -170,27 +213,21 @@ class Game():
                 self._answer('unauthorized%', client_socket)
             else:
                 if self._get_player_id(uuid) == -1 and msg.startswith("join%"):
-                    self._answer(self._new_player(
-                        uuid, msg.split("%")[1], client_thread.ip), client_socket)
+                    self._answer(self._new_player(uuid, msg.split("%")[1], client_thread.ip), client_socket)
         elif self.state == 2:
             if msg == "status%":
-                self._answer(
-                    f"start{self.game_data['letters']}", client_socket)
+                self._answer(f"start{self.game_data['letters']}", client_socket)
                 if "" == self.game_data["players"][self._get_player_id(uuid)]["word"]:
-                    self.game_data["players"][self._get_player_id(
-                        uuid)]["status"] = "Playing"
+                    self.game_data["players"][self._get_player_id(uuid)]["status"] = "Playing"
             elif msg.startswith("join%"):
                 self._answer("started", client_socket)
             elif not self._get_player_id(uuid) == -1:
                 if self.game_data["players"][self._get_player_id(uuid)]["status"] != "Finished":
                     if word_check.check_dict(msg, self.LANGUAGE) and word_check.check_list(msg, self.game_data["letters"]):
                         self._answer("valid%", client_socket)
-                        self.game_data["players"][self._get_player_id(
-                            uuid)]["status"] = "Finished"
-                        self.game_data["players"][self._get_player_id(
-                            uuid)]["word"] = msg
-                        print(
-                            f'[I] {self.game_data["players"][self._get_player_id(uuid)]["name"]} finished')
+                        self.game_data["players"][self._get_player_id(uuid)]["status"] = "Finished"
+                        self.game_data["players"][self._get_player_id(uuid)]["word"] = msg
+                        print(f'[I] {self.game_data["players"][self._get_player_id(uuid)]["name"]} finished')
                         all_finished = True
                         for i in self.game_data["players"]:
                             if i["status"] != "Finished" and not i["computer_afk"]:
@@ -198,6 +235,7 @@ class Game():
                                 break
                         if all_finished:
                             self.state = 3
+                            self._calculate_points()
                     else:
                         self._answer("invalid", client_socket)
                 else:
@@ -242,7 +280,7 @@ class MainThread(threading.Thread):
     def run(self):
         try:
             print("[I] Server started, listening...")
-            print("[V] Use portmapper for UPnP : link port 11111 to 11111. ")
+            #print("[V] Use portmapper for UPnP : link port 11111 to 11111. ")
             self.tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.tcpsock.bind(("", self.port))
