0f7baf93ee6b364972eee79961e45d7287d9921f
Alexis Rossfelder <contact.liteapplication@gmail.com>
2021-10-31 15:52:09 +0100

    Refactor DefaultSettingsProvider into SettingsProvider and DefaultProvider All settings provider should inherit from SettingsProvider and implement _load(self)->bool

--- src/server/settings.py
+++ src/server/settings.py
@@ -1,21 +1,13 @@
 from typing import Any, Tuple
 
 
-class DefaultSettingsProvider:
+class SettingsProvider:
     def __init__(self) -> None:
-        if not self.load():
+        if not self._load():
             raise RuntimeError("Could not load configuration")
 
-    def load(self):
-        self.settings = {
-            "server_port": 8080,
-            "server_address": "0.0.0.0",
-            "web_path": "src/web",
-            "letter_number": 7,
-            "dict_path": "src/dict/fr.txt",
-            "time_inactive": 3,
-        }
-        return True  # return False if something wen wrong when loading the config
+    def _load(self):
+        return False
 
     def __getitem__(self, key: str) -> Any:
         return self.settings[key]
@@ -28,4 +20,14 @@ class DefaultSettingsProvider:
         return self.settings["server_port"]
 
 
-DefaultProvider = DefaultSettingsProvider
+class DefaultProvider(SettingsProvider):
+    def _load(self):
+        self.settings = {
+            "server_port": 8080,
+            "server_address": "0.0.0.0",
+            "web_path": "src/web",
+            "letter_number": 7,
+            "dict_path": "src/dict/fr.txt",
+            "time_inactive": 3,
+        }
+        return True  # return False if something wen wrong when loading the config
