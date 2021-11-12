fbaacc31e6cb63be5e32388b1ba718ab92fa5b1a
Fox <fxcourel@gmail.com>
2021-11-12 15:08:28 +0100

    Login.html

--- src/web/js/login.js
+++ src/web/js/login.js
@@ -1,4 +1,24 @@
+function setContent(content) {
+    console.log('Changing content to', content);
+    if (content === 'login' && (document.getElementById('login').style.display === 'none' || document.getElementById('signin').style.display === '')) {
+        console.log('Changed content to login');
+        document.getElementById('signin').style.display = 'none';
+        document.getElementById('login').style.display = 'block';
+        document.getElementById('title_1').style.borderBottomStyle = 'none';
+        document.getElementById('title_2').style.borderBottomStyle = 'solid';
+    }
+    else if (content === 'signin' && (document.getElementById('signin').style.display === 'none' || document.getElementById('signin').style.display === '')) {
+        console.log('Changed content to signin');
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('signin').style.display = 'block';
+        document.getElementById('title_2').style.borderBottomStyle = 'none';
+        document.getElementById('title_1').style.borderBottomStyle = 'solid';
+    }
+}
+
+
 function main() {
+    setContent('login')
     window.onload = function() {
         document.getElementById("loading").style.display = 'none';
     };
