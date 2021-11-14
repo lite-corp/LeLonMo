038402d1f930212cbcd8e4c69ed39eddd02fdbd2
Alexis Rossfelder <contact.liteapplication@gmail.com>
2021-11-14 13:59:54 +0100

    Make use of js for button effect

--- src/web/js/login.js
+++ src/web/js/login.js
@@ -1,4 +1,4 @@
-function setContent(content) {
+function set_content(content) {
     console.log('Changing content to', content);
     if (content === 'login' && (document.getElementById('login').style.display === 'none' || document.getElementById('signin').style.display === '')) {
         console.log('Changed content to login');
@@ -6,8 +6,7 @@ function setContent(content) {
         document.getElementById('login').style.display = 'block';
         document.getElementById('title_1').style.borderBottomStyle = 'none';
         document.getElementById('title_2').style.borderBottomStyle = 'solid';
-    }
-    else if (content === 'signin' && (document.getElementById('signin').style.display === 'none' || document.getElementById('signin').style.display === '')) {
+    } else if (content === 'signin' && (document.getElementById('signin').style.display === 'none' || document.getElementById('signin').style.display === '')) {
         console.log('Changed content to signin');
         document.getElementById('login').style.display = 'none';
         document.getElementById('signin').style.display = 'block';
@@ -18,7 +17,20 @@ function setContent(content) {
 
 
 function main() {
-    setContent('login')
+    set_content('login')
+
+
+
+    var buttons = document.getElementsByClassName("validation_btn");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function(event) {
+            event.target.classList.add("active");
+            setTimeout(() => {
+                event.target.classList.remove("active");
+            }, 400);
+        })
+    }
+
     window.onload = function() {
         document.getElementById("loading").style.display = 'none';
     };
