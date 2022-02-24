57dbb3f799b63c00e51aa477681c7afe1a2b6faa
Alexis Rossfelder <contact.liteapplication@gmail.com>
2022-02-24 22:49:41 +0100

    Warn users about new messages

--- src/web/js/keepalive.js
+++ src/web/js/keepalive.js
@@ -1,3 +1,5 @@
+var message_received = false;
+
 function send_data(page, data, callback) {
     var xhr = new XMLHttpRequest();
 
@@ -14,7 +16,35 @@ function send_data(page, data, callback) {
 }
 
 function update() {
-    send_data('/llm', { 'action': 'update' }, (a, b) => {})
+    send_data('/llm', { 'action': 'update' }, (status, data) => {
+        if (status == 200 && data["success"]) {
+            if (data["should_update_messages"] && !message_received) {
+                toast("You have new messages in chat, click <a href=\"/html/index.html\">here</a> to see them", true, 10);
+                message_received = true;
+            }
+        }
+    })
+}
+
+function toast(text, blink, duration = 5) {
+    var toast_element = document.createElement("div")
+    toast_element.classList.add('notif')
+    toast_element.innerHTML = text
+    if (blink) {
+        toast_element.classList.add('blink')
+    }
+    page = document.getElementById("page")
+    page.appendChild(toast_element)
+    setTimeout(() => {
+        toast_element.classList.add("active")
+    }, 50);
+    setTimeout(() => {
+        toast_element.classList.toggle("active")
+        setTimeout(() => {
+            page.removeChild(toast_element)
+        }, 2000)
+    }, duration * 1000)
+
 }
 
 setInterval(update, 1000);
