e85b350c36620319aba43e4ff29ed2486467285d
Fox <fxcourel@gmail.com>
2022-03-31 22:26:12 +0200

    Revert "add multiple language (Beta)"
    This reverts commit 1cab0d096f2e771a5f42b2bf72b6ac85b45121c1.

--- src/web/js/language.js
+++
@@ -1,9 +0,0 @@
-function toggleLanguage(language) {
-    let description = document.getElementById("description");
-    if (language === "English") {
-      description.innerHTML = "Show English text";
-    }
-    else {
-      description.innerHTML = "Montre du texte Français";
-    }
-  }
